7bd716d31a528db090d0d6469d244b86af82b76a
Daniel Passos <daniel@passos.me>
2015-08-11 14:35:00 -0300

    Replace AeroGear code snippet by FH

--- admin-ui/app/snippets/register-device/android.java
+++ admin-ui/app/snippets/register-device/android.java
@@ -1,54 +1,31 @@
 package com.push.pushapplication;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import android.widget.Toast;
 
-import org.jboss.aerogear.android.unifiedpush.PushConfig;
-import org.jboss.aerogear.android.unifiedpush.PushRegistrar;
-import org.jboss.aerogear.android.unifiedpush.Registrations;
-
+import com.feedhenry.sdk.FH;
+import com.feedhenry.sdk.FHActCallback;
+import com.feedhenry.sdk.FHResponse;
 import android.app.Application;
 
 public class PushApplication extends Application {
-
-    private final String VARIANT_ID       = "{{ variant.variantID }}";
-    private final String SECRET           = "{{ variant.secret }}";
-    private final String GCM_SENDER_ID    = "{{ variant.projectNumber }}";
-    private final String UNIFIED_PUSH_URL = "{{ contextPath }}";
-
-    private PushRegistrar registration;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        Registrations registrations = new Registrations();
+        // Be sure run it after init()
+        FH.pushRegister(new FHActCallback() {
+            @Override
+            public void success(FHResponse fhResponse) {
+                // Do something cool
+            }
 
-        try {
-            PushConfig config = new PushConfig(new URI(UNIFIED_PUSH_URL), GCM_SENDER_ID);
-            config.setVariantID(VARIANT_ID);
-            config.setSecret(SECRET);
-            config.setAlias(MY_ALIAS);
+            @Override
+            public void fail(FHResponse fhResponse) {
+                Toast.makeText(getApplicationContext(),
+                        fhResponse.getErrorMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
-            registration = registrations.push("unifiedpush", config);
-
-            registration.register(getApplicationContext(), new Callback() {
-                private static final long serialVersionUID = 1L;
-
-                @Override
-                public void onSuccess(Void ignore) {
-                     Toast.makeText(MainActivity.this, "Registration Succeeded!",
-                             Toast.LENGTH_LONG).show();
-               }
-
-               @Override
-               public void onFailure(Exception exception) {
-                     Log.e("MainActivity", exception.getMessage(), exception);
-               }
-            });
-
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
